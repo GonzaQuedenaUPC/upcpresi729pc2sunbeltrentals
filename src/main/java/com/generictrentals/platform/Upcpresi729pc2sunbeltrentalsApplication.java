bffafabdbcade5526f492a38e179b74657bd598c
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-03 23:37:29 -0500

    chore: project setup.

---
+++ src/main/java/com/generictrentals/platform/Upcpresi729pc2sunbeltrentalsApplication.java
@@ -0,0 +1,13 @@
+package com.generictrentals.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Upcpresi729pc2sunbeltrentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Upcpresi729pc2sunbeltrentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generictrentals/platform/Upcpresi729pc2sunbeltrentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generictrentals.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upcpresi729pc2sunbeltrentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
