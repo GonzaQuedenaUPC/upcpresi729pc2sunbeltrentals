763f8b80a885e2e0996101444e0ad2f4e540c749
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-05 19:30:20 -0500

    feat(inventories): add commands.

---
+++ src/main/java/com/genericstrentals/platform/inventories/domain/model/commands/CreateCategoryCommand.java
@@ -0,0 +1,11 @@
+package com.genericstrentals.platform.inventories.domain.model.commands;
+
+/***
+ * CreateCategoryCommand
+ * @param name The name of the category.
+ * @param groupId The group to which the category belongs.
+ * @param description The description of the category.
+ * @param referenceImageUrl The URL of the image that represents the category.
+ */
+public record CreateCategoryCommand(String name, Long groupId, String description, String referenceImageUrl) {
+};
